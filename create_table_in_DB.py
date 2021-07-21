1ebf1bf27c9e7df4f227b2e1d79d518de64642bd
Guilherme Brasil Pintarelli <gp1500@gmail.com>
2021-07-21 12:02:05 -0300

    up

--- create_table_in_DB.py
+++ create_table_in_DB.py
@@ -41,6 +41,8 @@ E96_RES_TABLE= [1.00,	1.02,	1.05,
 8.66,	8.87,	9.09,
 9.31,	9.53,	9.76]
 
+num_decades = 12 # From 1m to 100M ohm
+
 PREC_TABLE= ["1", "5"] # Percentual values
 
 POWER_TABLE= ["1/10", "1/8", "1/4"] # Watts values
@@ -56,29 +58,49 @@ import pyodbc
 
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\AlitumLibrary.accdb;')
 cursor = conn.cursor()
-cursor.execute('select * from Resistor_SMD_0603')
+cursor.execute('select * from Resistor_SMD')
 
 
 # Print existing data in the DB   
 for row in cursor.fetchall():
     print (row)
 
-pos_prec_table = 1
-pos_power_table = 1
-pos_foot_table = 1
+pos_prec_table = 0
+pos_power_table = 0
+pos_foot_table = 0
+id_DB = 1
 
 #Sample insert
-for id_loop_res_value in range (len(E96_RES_TABLE)):
-    print(id_loop_res_value+1)
-    #cursor.execute("insert into Resistor_SMD_0603([ID],[Value]) values(?, 1)", (data, ))
-    #cursor.execute("insert into Resistor_SMD_0603([ID],[Comment]) values(?, ?)", (data, E96_RES_TABLE[id_loop]))
-    res_value = E96_RES_TABLE[id_loop_res_value]
-    make_description = 'RES SMD ' + str(res_value) + ' \u03A9 ' + '\u00B1' + PREC_TABLE[pos_prec_table] + '% ' + POWER_TABLE[pos_power_table] + 'W'
-    foot1_ref = FOORPRINT_REF_TABLE[pos_foot_table]
-    foot2_ref = FOORPRINT_REF_TABLE[pos_foot_table+1]
-    foot3_ref = FOORPRINT_REF_TABLE[pos_foot_table+2]
-    cursor.execute("insert into Resistor_SMD_0603([ID],[Description],[Comment],[Library Ref],[Footprint Ref 1],[Footprint Ref 2],[Footprint Ref 3]) values(?, ?, ?, ?, ?, ?, ?)", (id_loop_res_value+1, make_description, res_value, lib_ref, foot1_ref, foot2_ref, foot3_ref))
+for res_scale_rage in range(num_decades-1):
+    for loop_res_value in range (len(E96_RES_TABLE)):
+        print(id_DB)
+        #cursor.execute("insert into Resistor_SMD([ID],[Value]) values(?, 1)", (data, ))
+        #cursor.execute("insert into Resistor_SMD([ID],[Comment]) values(?, ?)", (data, E96_RES_TABLE[id_loop]))
+        
+        #res_value = E96_RES_TABLE[loop_res_value]
+        
+        # 1m, 10m, 100m (1m to 999m)
+        if res_scale_rage < 3: 
+            res_value = str(E96_RES_TABLE[loop_res_value]*10**res_scale_rage) + 'm'
 
+        # 1, 10, 100 (1 to 999R)
+        elif res_scale_rage >= 3 and res_scale_rage < 6: 
+            res_value = str(E96_RES_TABLE[loop_res_value]*10**res_scale_rage-3) + 'R'
+
+        # 1k, 10k, 1000k (1k to 999k)
+        elif res_scale_rage >= 6 and res_scale_rage < 9: 
+            res_value = str(E96_RES_TABLE[loop_res_value]*10**res_scale_rage-6) + 'k'
+
+        # 1M, 10M, 100M (1M to 100M)
+        elif res_scale_rage >= 6 and res_scale_rage < 9: 
+            res_value = str(E96_RES_TABLE[loop_res_value]*10**res_scale_rage-9) + 'M'
+        
+        make_description = 'RES SMD ' + str(res_value) + ' \u03A9 ' + '\u00B1' + PREC_TABLE[pos_prec_table] + '% ' + POWER_TABLE[pos_power_table] + 'W'
+        foot1_ref = FOORPRINT_REF_TABLE[pos_foot_table]
+        foot2_ref = FOORPRINT_REF_TABLE[pos_foot_table+1]
+        foot3_ref = FOORPRINT_REF_TABLE[pos_foot_table+2]
+        cursor.execute("insert into Resistor_SMD([ID],[Description],[Comment],[Library Ref],[Footprint Ref 1],[Footprint Ref 2],[Footprint Ref 3]) values(?, ?, ?, ?, ?, ?, ?)", (id_DB, make_description, res_value, lib_ref, foot1_ref, foot2_ref, foot3_ref))
+        id_DB=id_DB+1
 cursor.commit()
 
 #
@@ -87,7 +109,7 @@ cursor.commit()
 # Print existing data in the DB   
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\AlitumLibrary.accdb;')
 cursor = conn.cursor()
-cursor.execute('select * from Resistor_SMD_0603')
+cursor.execute('select * from Resistor_SMD')
 for row in cursor.fetchall():
     print (row)
 
